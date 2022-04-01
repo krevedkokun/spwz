5b0eb801f35ea13ab0608885561b167b6dc4b89d
KGOH <kgofhedgehogs@gmail.com>
2022-04-01 13:38:52 +0200

    Add profile & slicing example

--- src/spwz/core.clj
+++ src/spwz/core.clj
@@ -14,7 +14,7 @@
 #_(zen.dev/stop ztx)
 
 
-(zen.core/get-symbol ztx 'my-zen-ns/Patient)
+(zen.core/get-symbol ztx 'my-stuff.my-schemas/abstract-resource)
 
 
 (zen.core/errors ztx)
@@ -31,11 +31,22 @@
 
 (defn validate-resource [ztx resource]
   (let [rt                           (:resourceType resource)
+        profile-uris                 (set (get-in resource [:meta :profile]))
+
+        profile-schema-symbols               (zen.core/get-tag ztx 'my-stuff.my-schemas/profile-schema)
+        this-resource-profile-schema-symbols (filter (fn [sym] (contains?
+                                                                 profile-uris
+                                                                 (:profileUri (zen.core/get-symbol ztx sym))))
+                                                     profile-schema-symbols)
+
+
         persistent-schema-symbols    (zen.core/get-tag ztx 'my-zen-ns/persistent-resource)
         this-resource-schema-symbols (filter (fn [sym] (= rt (:resourceType (zen.core/get-symbol ztx sym))))
                                              persistent-schema-symbols)]
     (zen.core/validate ztx
-                       this-resource-schema-symbols
+                       (concat
+                         this-resource-profile-schema-symbols
+                         this-resource-schema-symbols)
                        resource)))
 
 
@@ -48,4 +59,25 @@
    :active true})
 
 
-(zen.core/get-tag ztx 'my-zen-ns/persistent-resource)
+(zen.core/get-tag ztx 'my-stuff.my-schemas/persistent-resource)
+
+
+(zen.core/get-tag ztx 'my-stuff.my-schemas/profile-schema)
+
+
+(validate-resource
+  ztx
+  {:id "pr1"
+   :resourceType "Practitioner"
+   :meta {:profile ["zen-demo-profiles:practitioner"]}
+   :name [{:given  ["John"]
+           :family "Doe"}]
+   :telecom [{:system "phone"
+              :use    "work"
+              :value  "8800555"}
+             {:system "email"
+              :use    "work"
+              :value "john.doe@example.com"}
+             {:system "phone"
+              :use    "mobile"
+              :value  "88005553535"}]})
