508f5e6cc143bf51916c9c2e10b8cc6761645985
KGOH <kgofhedgehogs@gmail.com>
2022-04-01 17:10:07 +0200

    Add slicing in slicing example

--- src/spwz/core.clj
+++ src/spwz/core.clj
@@ -14,34 +14,22 @@
 #_(zen.dev/stop ztx)
 
 
-(zen.core/get-symbol ztx 'my-stuff.my-schemas/abstract-resource)
-
-
 (zen.core/errors ztx)
-
-
-(zen.core/validate ztx
-                   #{'my-zen-ns/Patient}
-                   {:id "pt1"
-                    :resourceType "Patient"
-                    :name [{:given ["Yurii"]
-                            :family "Doe"}]
-                    :active true})
 
 
 (defn validate-resource [ztx resource]
   (let [rt                           (:resourceType resource)
         profile-uris                 (set (get-in resource [:meta :profile]))
 
-        profile-schema-symbols               (zen.core/get-tag ztx 'my-stuff.my-schemas/profile-schema)
+        profile-schema-symbols               (zen.core/get-tag ztx 'zen.fhir/profile-schema)
         this-resource-profile-schema-symbols (filter (fn [sym] (contains?
                                                                  profile-uris
-                                                                 (:profileUri (zen.core/get-symbol ztx sym))))
+                                                                 (:zen.fhir/profileUri (zen.core/get-symbol ztx sym))))
                                                      profile-schema-symbols)
 
 
-        persistent-schema-symbols    (zen.core/get-tag ztx 'my-zen-ns/persistent-resource)
-        this-resource-schema-symbols (filter (fn [sym] (= rt (:resourceType (zen.core/get-symbol ztx sym))))
+        persistent-schema-symbols    (zen.core/get-tag ztx 'zen.fhir/base-schema)
+        this-resource-schema-symbols (filter (fn [sym] (= rt (:zen.fhir/type (zen.core/get-symbol ztx sym))))
                                              persistent-schema-symbols)]
     (zen.core/validate ztx
                        (concat
@@ -50,35 +38,27 @@
                        resource)))
 
 
-(validate-resource
-  ztx
-  {:id "pt1"
-   :resourceType "Patient"
-   :name [{:given  ["Yurii"]
-           :family "Doe"}]
-   :active true})
-
-
 (zen.core/get-tag ztx 'my-stuff.my-schemas/persistent-resource)
 
 
 (zen.core/get-tag ztx 'my-stuff.my-schemas/profile-schema)
 
 
+(zen.core/get-symbol ztx 'my-zen-ns/PractitionerProfile)
+
+
 (validate-resource
   ztx
-  {:id "pr1"
+  {:id           "pr1"
    :resourceType "Practitioner"
-   :meta {:profile ["zen-demo-profiles:practitioner"]}
-   :name [{:given  ["John"]
-           :family "Doe"}]
-   :identifier []
-   :telecom [{:system "phone"
-              :use    "work"
-              :value  "8800555"}
-             {:system "email"
-              :use    "work"
-              :value "john.doe@example.com"}
-             {:system "phone!"
-              :use    "mobile"
-              :value  "88005553535"}]})
+   :meta         {:profile ["zen-demo-profiles:practitioner"]}
+   :name         [{:given  ["John"], :family "Doe"}]
+   :telecom      [{:system "phone"
+                   :use    "work"
+                   :value  "8800555"}
+                  {:system "email"
+                   :use    "work"
+                   :value  "john.doe@example.com"}
+                  {:system "pager"
+                   :use    "home"
+                   :value  "22222"}]})
